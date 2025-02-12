2de0a2f78f61f39429c451ce521b780ea89b0ec4
Rizkommam111 <87519254+Rizkommam111@users.noreply.github.com>
2025-02-12 21:10:17 +0700

    Update index.php mengubah komentar menjadi bahasa indonesia

--- index.php
+++ index.php
@@ -2,29 +2,29 @@
 
 /*
  *
- * File ini bagian dari:
+ * File ini adalah bagian dari:
  *
  * OpenSID
  *
- * Sistem informasi desa sumber terbuka untuk memajukan desa
+ * Sistem Informasi Desa sumber terbuka untuk memajukan desa
  *
- * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ * Aplikasi dan kode sumber ini dirilis berdasarkan lisensi GPL V3
  *
  * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
  * Hak Cipta 2016 - 2024 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
  *
  * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
  * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
- * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
- * asal tunduk pada syarat berikut:
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah, dan/atau mendistribusikan,
+ * selama mematuhi syarat berikut:
  *
  * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
- * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * setiap salinan atau bagian penting dari Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
  * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
  *
  * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
- * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
- * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN, ATAU
+ * KEWAJIBAN APA PUN YANG TIMBUL DARI PENGGUNAAN ATAU HAL LAINNYA TERKAIT APLIKASI INI.
  *
  * @package   OpenSID
  * @author    Tim Pengembang OpenDesa
@@ -37,30 +37,30 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * KONFIGURASI LINGKUNGAN APLIKASI
  *---------------------------------------------------------------
  *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
+ * Anda dapat memuat konfigurasi yang berbeda tergantung pada lingkungan yang digunakan.
+ * Pengaturan lingkungan juga mempengaruhi log dan pelaporan kesalahan.
  *
- * This can be set to anything, but default usage is:
+ * Nilai yang dapat digunakan:
  *
- *     development
- *     testing
- *     production
+ *     development (pengembangan)
+ *     testing (pengujian)
+ *     production (produksi)
  *
- * NOTE: If you change these, also change the error_reporting() code below
+ * CATATAN: Jika Anda mengubah ini, pastikan juga mengubah kode error_reporting() di bawah.
  */
 define('ENVIRONMENT', $_SERVER['CI_ENV'] ?? 'production');
 
 /*
  *---------------------------------------------------------------
- * ERROR REPORTING
+ * PELAPORAN KESALAHAN
  *---------------------------------------------------------------
  *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
+ * Setiap lingkungan memerlukan tingkat pelaporan kesalahan yang berbeda.
+ * Secara default, mode pengembangan akan menampilkan semua kesalahan,
+ * sedangkan mode pengujian dan produksi akan menyembunyikannya.
  */
 switch (ENVIRONMENT) {
     case 'development':
@@ -76,209 +76,47 @@ switch (ENVIRONMENT) {
 
     default:
         header('HTTP/1.1 503 Service Unavailable.', true, 503);
-        echo 'The application environment is not set correctly.';
-
-        exit(1); // EXIT_ERROR
+        echo 'Lingkungan aplikasi tidak dikonfigurasi dengan benar.';
+        exit(1); // KELUAR DENGAN ERROR
 }
 
 /*
  *---------------------------------------------------------------
- * SYSTEM DIRECTORY NAME
+ * NAMA DIREKTORI SISTEM
  *---------------------------------------------------------------
  *
- * This variable must contain the name of your "system" directory.
- * Set the path if it is not in the same directory as this file.
+ * Variabel ini harus berisi nama direktori "system".
+ * Atur jalurnya jika tidak berada dalam direktori yang sama dengan file ini.
  */
 $system_path = 'vendor/codeigniter/framework/system';
 
 /*
  *---------------------------------------------------------------
- * APPLICATION DIRECTORY NAME
+ * NAMA DIREKTORI APLIKASI
  *---------------------------------------------------------------
  *
- * If you want this front controller to use a different "application"
- * directory than the default one you can set its name here. The directory
- * can also be renamed or relocated anywhere on your server. If you do,
- * use an absolute (full) server path.
- * For more info please see the user guide:
- *
- * https://codeigniter.com/userguide3/general/managing_apps.html
- *
- * NO TRAILING SLASH!
+ * Jika Anda ingin menggunakan direktori "application" yang berbeda dari default,
+ * Anda dapat mengaturnya di sini. Direktori ini juga dapat diubah nama atau dipindahkan
+ * ke lokasi lain di server Anda. Jika Anda melakukannya, gunakan jalur absolut (penuh).
  */
 $application_folder = 'donjo-app';
 
 /*
  *---------------------------------------------------------------
- * VIEW DIRECTORY NAME
+ * NAMA DIREKTORI VIEW
  *---------------------------------------------------------------
  *
- * If you want to move the view directory out of the application
- * directory, set the path to it here. The directory can be renamed
- * and relocated anywhere on your server. If blank, it will default
- * to the standard location inside your application directory.
- * If you do move this, use an absolute (full) server path.
- *
- * NO TRAILING SLASH!
+ * Jika Anda ingin memindahkan direktori tampilan (view) ke luar dari direktori aplikasi,
+ * atur jalurnya di sini. Direktori ini dapat diubah nama atau dipindahkan ke lokasi lain di server.
+ * Jika dikosongkan, akan menggunakan lokasi standar dalam direktori aplikasi.
  */
 $view_folder = '';
 
 /*
  * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
+ * MUAT FILE BOOTSTRAP
  * --------------------------------------------------------------------
  *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here. For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT: If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller. Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
- */
-// The directory name, relative to the "controllers" directory.  Leave blank
-// if your controller is not in a sub-directory within the "controllers" one
-// $routing['directory'] = '';
-
-// The controller class file name.  Example:  mycontroller
-// $routing['controller'] = '';
-
-// The controller function you wish to be called.
-// $routing['function']	= '';
-
-/*
- * -------------------------------------------------------------------
- *  CUSTOM CONFIG VALUES
- * -------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed dynamically to the
- * config class when initialized. This allows you to set custom config
- * items or override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
- */
-// $assign_to_config['name_of_config_item'] = 'value of config item';
-
-// --------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
-
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
- */
-
-// Set the current directory correctly for CLI requests
-if (defined('STDIN')) {
-    chdir(__DIR__);
-}
-
-if (($_temp = realpath($system_path)) !== false) {
-    $system_path = $_temp . DIRECTORY_SEPARATOR;
-} else {
-    // Ensure there's a trailing slash
-    $system_path = strtr(
-        rtrim($system_path, '/\\'),
-        '/\\',
-        DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-    ) . DIRECTORY_SEPARATOR;
-}
-
-// Is the system path correct?
-if (! is_dir($system_path)) {
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: ' . pathinfo(__FILE__, PATHINFO_BASENAME);
-
-    exit(3); // EXIT_CONFIG
-}
-
-/*
- * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
- * -------------------------------------------------------------------
- */
-// The name of THIS file
-define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-// Path to the system directory
-define('BASEPATH', $system_path);
-
-// Path to the front controller (this file) directory
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
-
-// Name of the "system" directory
-define('SYSDIR', basename(BASEPATH));
-
-// The path to the "application" directory
-if (is_dir($application_folder)) {
-    if (($_temp = realpath($application_folder)) !== false) {
-        $application_folder = $_temp;
-    } else {
-        $application_folder = strtr(
-            rtrim($application_folder, '/\\'),
-            '/\\',
-            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-        );
-    }
-} elseif (is_dir(BASEPATH . $application_folder . DIRECTORY_SEPARATOR)) {
-    $application_folder = BASEPATH . strtr(
-        trim($application_folder, '/\\'),
-        '/\\',
-        DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-    );
-} else {
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: ' . self;
-
-    exit(3); // EXIT_CONFIG
-}
-
-define('APPPATH', $application_folder . DIRECTORY_SEPARATOR);
-
-// The path to the "views" directory
-if (! isset($view_folder[0]) && is_dir(APPPATH . 'views' . DIRECTORY_SEPARATOR)) {
-    $view_folder = APPPATH . 'views';
-} elseif (is_dir($view_folder)) {
-    if (($_temp = realpath($view_folder)) !== false) {
-        $view_folder = $_temp;
-    } else {
-        $view_folder = strtr(
-            rtrim($view_folder, '/\\'),
-            '/\\',
-            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-        );
-    }
-} elseif (is_dir(APPPATH . $view_folder . DIRECTORY_SEPARATOR)) {
-    $view_folder = APPPATH . strtr(
-        trim($view_folder, '/\\'),
-        '/\\',
-        DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-    );
-} else {
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: ' . self;
-
-    exit(3); // EXIT_CONFIG
-}
-
-define('VIEWPATH', $view_folder . DIRECTORY_SEPARATOR);
-define('FORCE_HTTPS', false);
-
-/*
- * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
- * --------------------------------------------------------------------
- *
- * And away we go...
+ * Dan jalankan aplikasi...
  */
 require_once BASEPATH . 'core/CodeIgniter.php';
